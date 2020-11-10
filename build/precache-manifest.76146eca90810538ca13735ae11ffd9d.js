57de7ac1c36a63a349c1546fb7393220056342ea
Revanth Raghu <revabob@gmail.com>
2020-11-10 14:25:51 +0530

    Add files via upload

---
+++ build/precache-manifest.76146eca90810538ca13735ae11ffd9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79caaf83ad9507e932f9a2326d33557",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6a7af04ef765f43365f2",
+    "url": "./static/js/2.c638e50b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c638e50b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d82c14bbb01cee6133",
+    "url": "./static/js/main.8afc2eee.chunk.js"
+  },
+  {
+    "revision": "b606c38c310c2793a60c",
+    "url": "./static/js/runtime-main.206513d0.js"
+  }
+]);
